351dcd98fde05bd6674cbf3be72e7d7e25228a84
yumeow0122 <68141436+yumeow0122@users.noreply.github.com>
2022-06-15 13:23:20 +0800

    finish python set env

---
+++ After_Clone.bat
@@ -0,0 +1,3 @@
+docker run -it --rm -v %cd%:/tmp -w /tmp python:3 python After_Clone.py
+
+PAUSE
---
+++ After_Clone.py
@@ -0,0 +1,25 @@
+import re
+import shutil
+
+path = r"./Tiramisu_Laravel/laradock/.env.example"
+file = open (path, "r+")
+open('./Tiramisu_Laravel/laradock/.env', 'w').write(re.sub(r'PHP_VERSION=7.4', 'PHP_VERSION=8.1', file.read()))
+file.close()
+
+print("first one complete.\n")
+
+src=r'./Tiramisu_Laravel/.env.example'
+des=r'./Tiramisu_Laravel/.env'
+shutil.copy(src, des)
+
+path = r'./Tiramisu_Laravel/.env'
+with open(path, 'r') as file:
+    data = file.read()
+    data = data.replace('DB_HOST=127.0.0.1'  , 'DB_HOST=mysql')
+    data = data.replace('DB_DATABASE=laravel', 'DB_DATABASE=default')
+    data = data.replace('DB_PASSWORD='       , 'DB_PASSWORD=root')
+
+with open(path, 'w') as file:
+    file.write(data)
+
+print("all complete.\n")
