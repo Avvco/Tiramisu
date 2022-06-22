7d081eb9056281e53d0891a3e385ec05488bffba
yumeow0122 <yumeow0122@gmail.com>
2022-06-22 14:09:53 +0800

    add two port in afterClone.py

--- _After_Clone.py
+++ _After_Clone.py
@@ -18,6 +18,8 @@ with open(path, 'r') as file:
     data = data.replace('DB_HOST=127.0.0.1'  , 'DB_HOST=mysql')
     data = data.replace('DB_DATABASE=laravel', 'DB_DATABASE=default')
     data = data.replace('DB_PASSWORD='       , 'DB_PASSWORD=root')
+    data = data.replace('WORKSPACE_BROWSERSYNC_HOST_PORT=3000', 'WORKSPACE_BROWSERSYNC_HOST_PORT=64500')
+    data = data.replace('WORKSPACE_BROWSERSYNC_UI_HOST_PORT=3001', 'WORKSPACE_BROWSERSYNC_UI_HOST_PORT=64501')
 
 with open(path, 'w') as file:
     file.write(data)
