05f76b844a8f62f4d2666da9be486b86338e3524
yumeow0122 <yumeow0122@gmail.com>
2022-06-22 17:36:31 +0800

    python 2 -> 1

--- After_Clone.sh
+++ After_Clone.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 # Copy and Fill .env files in Tiramisu_Laravel and laradock
-docker run -it --rm -v $PWD:/tmp -w /tmp python:3 python _After_Clone1.py
-docker run -it --rm -v $PWD:/tmp -w /tmp python:3 python _After_Clone2.py
+docker run -it --rm -v $PWD:/tmp -w /tmp python:3 python _After_Clone.py
 
 # First Stop all running containers
 docker stop $(docker ps -a -q)
