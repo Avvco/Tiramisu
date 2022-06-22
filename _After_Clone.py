7e339a78152eb7db9c196480cc766ae35287d402
yumeow0122 <68141436+yumeow0122@users.noreply.github.com>
2022-06-22 15:40:10 +0800

    fix After_Clone.py

--- _After_Clone.py
+++ _After_Clone.py
@@ -1,12 +1,22 @@
 import re
 import shutil
 
-path = r"./Tiramisu_Laravel/laradock/.env.example"
-file = open (path, "r+")
-open('./Tiramisu_Laravel/laradock/.env', 'w').write(re.sub(r'PHP_VERSION=7.4', 'PHP_VERSION=8.1', file.read()))
-file.close()
+src=r'./Tiramisu_Laravel/laradock/.env.example'
+des=r'./Tiramisu_Laravel/laradock/.env'
+shutil.copy(src, des)
+
+path = r'./Tiramisu_Laravel/laradock/.env'
+with open(path, 'r') as file:
+    data = file.read()
+    data = data.replace('PHP_VERSION=7.4', 'PHP_VERSION=8.1')
+    data = data.replace('WORKSPACE_BROWSERSYNC_HOST_PORT=3000', 'WORKSPACE_BROWSERSYNC_HOST_PORT=64500')
+    data = data.replace('WORKSPACE_BROWSERSYNC_UI_HOST_PORT=3001', 'WORKSPACE_BROWSERSYNC_UI_HOST_PORT=64501')
+
+with open(path, 'w') as file:
+    file.write(data)
 
 print("first one complete.\n")
+
 
 src=r'./Tiramisu_Laravel/.env.example'
 des=r'./Tiramisu_Laravel/.env'
@@ -18,8 +28,6 @@ with open(path, 'r') as file:
     data = data.replace('DB_HOST=127.0.0.1'  , 'DB_HOST=mysql')
     data = data.replace('DB_DATABASE=laravel', 'DB_DATABASE=default')
     data = data.replace('DB_PASSWORD='       , 'DB_PASSWORD=root')
-    data = data.replace('WORKSPACE_BROWSERSYNC_HOST_PORT=3000', 'WORKSPACE_BROWSERSYNC_HOST_PORT=64500')
-    data = data.replace('WORKSPACE_BROWSERSYNC_UI_HOST_PORT=3001', 'WORKSPACE_BROWSERSYNC_UI_HOST_PORT=64501')
 
 with open(path, 'w') as file:
     file.write(data)
