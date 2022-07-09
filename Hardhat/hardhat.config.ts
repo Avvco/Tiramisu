af820ec776dd36d89fe6cdfcfe00daef1d462d75
yumeow0122 <yumeow0122@gmail.com>
2022-07-09 15:10:11 +0800

    rebuild all hardhat env

--- Hardhat/hardhat.config.ts
+++ Hardhat/hardhat.config.ts
@@ -1,37 +1,8 @@
-import { task } from "hardhat/config";
 import { HardhatUserConfig } from "hardhat/config";
-import "@nomiclabs/hardhat-ethers";
-import "@nomiclabs/hardhat-waffle";
-import "tsconfig-paths/register";
-import "hardhat-deploy"
-import "hardhat-deploy-ethers";
+import "@nomicfoundation/hardhat-toolbox";
 
-
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
- export default{
-  solidity: {
-    version: "0.8.5",
-    settings: {
-      optimizer: {
-        enabled: true,
-        runs: 1000,
-      },
-    },
-  },
+const config: HardhatUserConfig = {
+  solidity: "0.8.9",
 };
 
+export default config;
