d39b1485f3f685c7faad5b235b0cfe900ee75410
yumeow0122 <yumeow0122@gmail.com>
2022-08-08 01:03:43 +0800

    renew hardhat config change env from mnemonic to private key

--- Hardhat/src/IdentityAuthentication.sol
+++ Hardhat/src/IdentityAuthentication.sol
@@ -8,33 +8,26 @@ contract IdentityAuthentication{
 
   constructor() payable {}
 
-  function encode(int256 _personID, int256 _recordID, int256 _birthday, int256 _key) public payable returns (bool){
+  function encode(int256 _personID, int256 _recordID, int256 _birthday, int256 _key) public payable returns (int256, int256, int256){
 
     int256 coef0 = _key;
     int256 coef1 = random();
     int256 coef2 = random();
 
-    attribute[msg.sender][_personID] = encodePolynomial(coef0, coef1, coef2, _personID);
-    attribute[msg.sender][_recordID] = encodePolynomial(coef0, coef1, coef2, _recordID);
-    attribute[msg.sender][_birthday] = encodePolynomial(coef0, coef1, coef2, _birthday);
+    int256 _personIDVal = encodePolynomial(coef0, coef1, coef2, _personID);
+    int256 _recordIDVal = encodePolynomial(coef0, coef1, coef2, _recordID);
+    int256 _birthdayVal = encodePolynomial(coef0, coef1, coef2, _birthday);
 
-    return true;
+    return (_personIDVal, _recordIDVal, _birthdayVal);
   }
 
-  function decode(int256 _personID, int256 _recordID, int256 _birthday) public payable returns(int256) {
+  function decode(int256 _personID, int256 _recordID, int256 _birthday,
+                  int256 _personIDVal, int256 _recordIDVal, int256 _birthdayVal) public payable returns(int256) {
 
-    int256 personIDVal = getAttributeVal(_personID);
-    int256 recordIDVal = getAttributeVal(_recordID);
-    int256 birthdayVal = getAttributeVal(_birthday);
-
-    int256 key = decodePolynomial(personIDVal, _personID, _recordID, _birthday) +
-                 decodePolynomial(recordIDVal, _recordID, _personID, _birthday) +
-                 decodePolynomial(birthdayVal, _birthday, _personID, _recordID);
+    int256 key = decodePolynomial(_personIDVal, _personID, _recordID, _birthday) +
+                 decodePolynomial(_recordIDVal, _recordID, _personID, _birthday) +
+                 decodePolynomial(_birthdayVal, _birthday, _personID, _recordID);
     return key;
-  }
-
-  function getAttributeVal(int256 _attrName) private view returns (int256){
-    return attribute[msg.sender][_attrName];
   }
 
   function random() private view returns (int256) {
@@ -47,5 +40,5 @@ contract IdentityAuthentication{
 
   function decodePolynomial(int256 _y, int256 _x1, int256 _x2, int256 _x3) private pure returns (int256) {
     return _y * _x2 * _x3 / ((_x1 - _x2) * (_x1 - _x3));
-  } 
+  }
 } 
