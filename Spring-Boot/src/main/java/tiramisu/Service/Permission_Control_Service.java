9ba031de085900cb7131e12ccce7024392abd873
Avvco <leei910110@gmail.com>
2022-11-16 03:57:39 +0800

    update permision control

--- Spring-Boot/src/main/java/tiramisu/Service/Permission_Control_Service.java
+++ Spring-Boot/src/main/java/tiramisu/Service/Permission_Control_Service.java
@@ -1,10 +1,17 @@
 package tiramisu.Service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import tiramisu.DataBase.DAO.UserDAO;
 
 @Service
 public class Permission_Control_Service {
-  public static boolean permissionControl() {
+
+  @Autowired
+  public UserDAO userDAO;
+  
+  public boolean permissionControl() {
     return true;
   }
 }
