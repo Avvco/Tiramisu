a48880c26a1453aaabcbc4fa889cf19a52d51f44
Avvco <leei910110@gmail.com>
2022-11-16 03:55:01 +0800

    update user table

--- Spring-Boot/src/main/java/tiramisu/DataBase/DTO/User.java
+++ Spring-Boot/src/main/java/tiramisu/DataBase/DTO/User.java
@@ -1,15 +1,17 @@
 package tiramisu.DataBase.DTO;
 
-import java.sql.Date;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
   @Id
 	@GeneratedValue(strategy=GenerationType.AUTO)
@@ -17,7 +19,7 @@ public class User {
   
   private String user_name;
   private String id_number;
-  private Date birthday;
-  private Date register_date;
+  private String email;
+  private String type;
 
 }
