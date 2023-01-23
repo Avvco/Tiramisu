5e2111a778b0acd7ad0b6ff53d13b796c52bce03
yumeow0122 <yuemow0122@gamil.com>
2023-01-23 22:22:03 +0800

    Shit

--- Hardhat/src/MedicalStaff.sol
+++ Hardhat/src/MedicalStaff.sol
@@ -3,30 +3,60 @@ pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+
 import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
 
 // This is the main building block for smart contracts.
 
-contract MedicalStaff is Ownable, ERC721("MedicalStaff", "MS"){
-  using Counters for Counters.Counter;
-  Counters.Counter private _medicalAccountCounter;
+contract MedicalStaff is Ownable, ERC721("MedicalStaff", "MS") {
+    using Counters for Counters.Counter;
+    using Strings for uint256;
 
-  uint256 public constant MAX_SUPPLY = 10;
-  uint256 public constant MAX_BALANCE = 1;
+    Counters.Counter private _medicalAccountCounter;
 
-  function registerMedicalAccount()public onlyOwner{
-    require(balanceOf(msg.sender) <= MAX_BALANCE, "This account already been registe.");
+    uint256 public constant MAX_SUPPLY = 10;
+    uint256 public constant MAX_BALANCE = 1;
 
-    _mintForRegister();
-  }
+    string private constant BASE_URI = "test/";
+    string private constant BASE_Extension = ".json";
 
-  function isMedicalStaff() public view returns(bool) {
-    return (balanceOf(msg.sender) == 1);
-  }
+    mapping (uint256 => string) _tokenURIs;
 
-  function _mintForRegister()private {
-    uint256 tokenID = _medicalAccountCounter.current();
-    _medicalAccountCounter.increment();
-    _safeMint(msg.sender, tokenID);
-  }
+    function currentAccountID() public view returns (uint256) {
+        return _medicalAccountCounter.current();
+    }
+
+    function registerMedicalAccount() public onlyOwner {
+        require(
+            balanceOf(msg.sender) < MAX_BALANCE,
+            "This account already been registe."
+        );
+
+        _mintForRegister();
+    }
+
+    function getTokenURI(uint256 tokenID) public view virtual returns(string memory){
+        require(_exists(tokenID), "ERC721Metadata: URI query for nonexistent token.");
+
+        string memory tokenURI = _tokenURIs[tokenID];
+
+        if(bytes(BASE_URI).length == 0)
+            return tokenURI;
+
+        if(bytes(tokenURI).length > 0)
+            return string(abi.encodePacked(BASE_URI, tokenURI));
+        
+        return string(abi.encodePacked(BASE_URI, tokenID.toString(), BASE_Extension));
+    }
+
+    function isMedicalStaff() public view returns (bool) {
+        return (balanceOf(msg.sender) == 1);
+    }
+
+    function _mintForRegister() private {
+        uint256 tokenID = currentAccountID();
+        _medicalAccountCounter.increment();
+        _safeMint(msg.sender, tokenID);
+    }
 }
