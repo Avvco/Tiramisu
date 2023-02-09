a2cc69009b947e2b15a22158bbc657d49872fe5c
yumeow0122 <yumeow0122@gmail.com>
2023-02-09 15:45:14 +0800

    Hardhat: *fix wrong direct

--- Hardhat/hardhatRun.sh
+++ Hardhat/hardhatRun.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 npx hardhat node
-
-cp ./address/medical-address.ts ../Angular/src/app/user/util/contract/address/medical-address.ts
-cp ./address/patient-address.ts ../Angular/src/app/user/util/contract/address/patient-address.ts
-cp ./address/verifier-address.ts ../Angular/src/app/user/util/contract/address/verifier-address.ts
+cp ./scripts/address/medical-address.ts ../Angular/src/app/user/util/contract/address/medical-address.ts
+cp ./scripts/address/patient-address.ts ../Angular/src/app/user/util/contract/address/patient-address.ts
+cp ./scripts/address/verifier-address.ts ../Angular/src/app/user/util/contract/address/verifier-address.ts
