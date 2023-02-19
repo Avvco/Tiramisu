5f21af9ad23acba473691b904755c565aebba037
Avvco <leei910110@gmail.com>
2023-02-19 18:17:29 +0800

    change annotation type

--- Spring-Boot/src/main/java/tiramisu/DataBase/DAO/UserDAO.java
+++ Spring-Boot/src/main/java/tiramisu/DataBase/DAO/UserDAO.java
@@ -1,19 +1,21 @@
 package tiramisu.DataBase.DAO;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Component;
 
 import tiramisu.DataBase.DTO.User;
-import tiramisu.DataBase.DTO.User_Authorization;
 import tiramisu.DataBase.DTO.User.UserType;
+import tiramisu.DataBase.DTO.User_Authorization;
 
 // https://www.bezkoder.com/jpa-repository-query/
 
-@Repository
+@Component
 public interface UserDAO extends JpaRepository<User, Integer> {
   List<User> findByUserNameAndEmailAndType(String userName, String email, UserType type);
   List<User> findByUserAuthorization(User_Authorization userAuthorization);
   List<User> findByType(UserType type);
+  Optional<User> findByUserNameAndType(String userName, UserType type);
 }
--- Spring-Boot/src/main/java/tiramisu/DataBase/DAO/User_AuthorizationDAO.java
+++ Spring-Boot/src/main/java/tiramisu/DataBase/DAO/User_AuthorizationDAO.java
@@ -3,11 +3,11 @@ package tiramisu.DataBase.DAO;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Component;
 
 import tiramisu.DataBase.DTO.User_Authorization;
 
-@Repository
+@Component
 public interface User_AuthorizationDAO extends JpaRepository<User_Authorization, Integer> {
   List<User_Authorization> findByUserAuthorizationId(String userAuthorizationId);
   List<User_Authorization> findByToken(String token);
